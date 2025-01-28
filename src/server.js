b994d4ccca4a63f2fe0e2cff244512882f070fbd
ManjuRamu <131784940+ManjuRamu@users.noreply.github.com>
2025-01-28 21:21:45 +0530

    @global error handler

--- src/server.js
+++ src/server.js
@@ -2,9 +2,15 @@ import express from 'express';
 import { logger } from './utils/loggers.js';
 import { ENV } from './utils/env.js';
 import { logRequest } from './middleware/logs.js';
+import  router  from './routes/index.js';
+import { globalErrorHandler } from './middleware/error.handler.js';
 export const app = new express();
-app.use(logRequest(ENV.LOG_REQUEST))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.use(logRequest(ENV.LOG_REQUEST))
+app.use("/api/v1",router)
+app.use(globalErrorHandler)
 app.listen(ENV.PORT, ENV.HOST, () => {
     logger.info("server is runnig", { port: ENV.PORT, host: ENV.HOST })
     logger.info(`http://${ENV.HOST}:${ENV.PORT}`)
