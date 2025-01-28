eb4e71ba8629a1220c9b61a83275b16d2716b80b
ManjuRamu <131784940+ManjuRamu@users.noreply.github.com>
2025-01-28 21:14:41 +0530

    @api error

---
+++ src/error/apiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error{
+    constructor(message, statusCode = 500, meta = {}) {
+        super(message);
+        this.name = this.constructor.name; 
+        this.message = message;
+        this.statusCode = statusCode; 
+        this.meta = meta;
+      }
+}
+
+class BadRequest extends ApiError{
+  constructor(message, meta){
+    super(message, 404,meta)
+  }
+}
