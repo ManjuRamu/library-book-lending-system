8dc0d0912706b9306e9d3ebab4443bd12f7340f0
ManjuRamu <131784940+ManjuRamu@users.noreply.github.com>
2025-01-28 22:02:25 +0530

    @add few books

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,26 @@
 export const users = [],
   barrowBooks = [],
   books = [
-    {
-      id: 1,
-      title: "Book A",
-      availableCopies: 2,
-    },
-    { id: 2, title: "Book B", availableCopies: 1 },
-    { id: 3, title: "Book C", availableCopies: 0 },
+    [
+        { "title": "The Adventures of Huckleberry Finn", "availableCopies": 2 },
+        { "title": "Moby Dick", "availableCopies": 3 },
+        { "title": "Pride and Prejudice", "availableCopies": 4 },
+        { "title": "The Great Gatsby", "availableCopies": 5 },
+        { "title": "To Kill a Mockingbird", "availableCopies": 3 },
+        { "title": "1984", "availableCopies": 6 },
+        { "title": "The Catcher in the Rye", "availableCopies": 2 },
+        { "title": "War and Peace", "availableCopies": 1 },
+        { "title": "The Odyssey", "availableCopies": 4 },
+        { "title": "Ulysses", "availableCopies": 3 },
+        { "title": "Brave New World", "availableCopies": 5 },
+        { "title": "Frankenstein", "availableCopies": 2 },
+        { "title": "The Lord of the Rings", "availableCopies": 7 },
+        { "title": "Crime and Punishment", "availableCopies": 4 },
+        { "title": "The Picture of Dorian Gray", "availableCopies": 3 },
+        { "title": "Wuthering Heights", "availableCopies": 2 },
+        { "title": "Jane Eyre", "availableCopies": 5 },
+        { "title": "Dracula", "availableCopies": 4 },
+        { "title": "Anna Karenina", "availableCopies": 3 },
+        { "title": "The Grapes of Wrath", "availableCopies": 2 }
+      ]      
   ];
